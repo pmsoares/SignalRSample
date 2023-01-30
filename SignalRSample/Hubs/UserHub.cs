64e9d873e6e6f9e7f2ad7919885b664cb4f05489
Pedro Soares <pmsoares@gmail.com>
2023-01-30 22:47:01 +0000

    Sessão de 30/01/2023

---
+++ SignalRSample/Hubs/UserHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            //enviar atualizações para todos os clientes
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+    }
+}
---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,27 @@
+﻿// Criar a ligação
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+// Ligar aos métodos que o hub invoca, isto é, receber as notificações do hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+// invocar os métodos do hub, isto é, enviar notificações para o hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+
+// iniciar a ligação
+function fulfilled() {
+    //fazer alguma coisa no início
+    console.log("Connection to UserHub successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    // rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
